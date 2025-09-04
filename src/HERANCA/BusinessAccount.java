658e01f721340718a3fc7555de8ccc57c8eda647
rodriguessavio <capar.2022113tads0049@aluno.ifpi.edu.br>
2025-09-04 18:37:43 -0300

    adding content about inheritance

---
+++ src/HERANCA/BusinessAccount.java
@@ -0,0 +1,28 @@
+package HERANCA;
+
+public class BusinessAccount extends Account{
+    private double loanLimit;
+
+    public BusinessAccount(){
+    }
+
+    public BusinessAccount(Integer number, String holder, double balance, double loanLimit){
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount){
+        if (amount <= loanLimit){
+            balance += amount - 10.0;
+        }
+    }
+
+}
